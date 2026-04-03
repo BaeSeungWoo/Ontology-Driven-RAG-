74a047a4268a508cd170650b4e75bf2d126c96f4
김성민 <ksm4949@gmail.com>
2026-04-03 11:08:37 +0900

    프론트엔드 기본골격 설정

---
+++ frontend/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  devIndicators: false, // 개발모드에서 뱃지 가시여부 옵션
+};
+
+export default nextConfig;
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Ontology-Driven-RAG",
+  description: "Ontology-Driven-RAG",
+  icons: {
+    icon: "/logo3.png",
+    shortcut: "/logo3.png",
+    apple: "/logo3.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
